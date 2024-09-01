729daec49ad9731e7032cb78120efc765eb53438
shubuzuo <shubuzuo@gmail.com>
2024-09-01 16:07:27 +0800

    feat: add validate and transform

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        strategy: 'excludeAll',
+        enableImplicitConversion: true,
+        excludeExtraneousValues: true,
+      },
+    }),
+  );
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
