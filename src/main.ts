da46bae907fbd49046ee6db6523b711011b2e257
shubuzuo <shubuzuo@gmail.com>
2024-09-01 16:13:19 +0800

    feat: generate openapi for ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,19 @@ async function bootstrap() {
     }),
   );
   app.enableCors();
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('openapi')
+    .setDescription('ebook 后端 api')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, {
+    deepScanRoutes: true,
+    operationIdFactory: (controllerKey: string, methodKey: string) =>
+      `${controllerKey.split('Controller')[0]}_${methodKey}`,
+  });
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
